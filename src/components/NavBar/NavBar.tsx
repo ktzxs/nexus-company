0bed00c13db3288e22b45ae4bd982cb70fa771d1
ktzxs <lucasalvesdacosta070@gmail.com>
2026-03-05 17:53:51 -0300

    primeiras edicoes mas com erro

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import MiddleNav from "./MiddleNav"
+
+export default function NavBar() {
+    return (
+        <>
+            <header className="w-full">
+                <div className="hidden lg:block">
+                    <MiddleNav />
+                </div>
+            </header>
+        </>
+    )
+}
